d953d32289e47b66fbf9446dbf499ca5aa033e3d
crimsonskyrem <crimsonsky.rem@gmail.com>
2021-12-28 17:58:15 +0800

    add func

--- kata-python/apuzzleaday.py
+++ kata-python/apuzzleaday.py
@@ -1,3 +1,5 @@
+from datetime import date
+LOCK_STRING = 'X'
 board = [
     ["JAN","FEB","MAR","APR","MAY","JUN"],
     ["JUL","AUG","SEP","OCT","NOV","DEC"],
@@ -10,39 +12,39 @@ board = [
     [" "  ," "  ,"THU","FRI","SAT","SUN"]
 ]
 bk1 = [
-    [1],
+    [1,0,0],
     [1,1,1],
-    [1],
-    [1]
+    [1,0,0],
+    [1,0,0]
 ]
 bk2 = [
-    [1],
+    [1,0],
     [1,1],
-    [1],
-    [1],
-    [1]
+    [1,0],
+    [1,0],
+    [1,0]
 ]
 bk3 = [
     [1,1],
-    [1],
-    [1],
-    [1]
+    [1,0],
+    [1,0],
+    [1,0]
 ]
 bk4 = [
     [1,1],
-    [1],
-    [1],
-    [1],
-    [1]
+    [1,0],
+    [1,0],
+    [1,0],
+    [1,0]
 ]
 bk5 = [
     [1,1,1],
-    [1],
-    [1],
-    [1]
+    [1,0,0],
+    [1,0,0],
+    [1,0,0]
 ]
 bk6 = [
-    [1],
+    [1,0],
     [1,1],
     [0,1],
     [0,1]
@@ -50,25 +52,54 @@ bk6 = [
 bk7 = [
     [1,0,1],
     [1,1,1],
-    [1]
+    [1,0,0]
 ]
 bk8 = [
-    [1],
+    [1,0,0],
     [1,1,1],
-    [0,1],
-    [0,1]
+    [0,1,0],
+    [0,1,0]
 ]
 bk9 = [
-    [1],
+    [1,0],
     [1,1],
     [1,1]
 ]
 
-def rotate(bk):
-    pass
-def chkByDate():
-    pass
+def rotate(bk, times= 0):
+    res = bk
+    if times < 0:
+        res = list(map(lambda x:x[::-1],res))
+        times += 1
+    for _ in range(abs(times)):
+        res = list(zip(*res[::-1]))
+    return res
+
+def printBk(bk):
+    for i in bk:
+        for j in i:
+            print(j,end=" ")
+        print()
+
+def markDateBoard():
+    today = date.today()
+    nboard = board
+    weekday = today.weekday()
+    for i in range(9):
+        for j in range(6):
+            if i < 2 and j + i*6 == today.month - 1:
+                nboard[i][j] = LOCK_STRING
+            if i > 1 and i < 8 and  j + (i-2)*6 == today.day - 1:
+                nboard[i][j] = LOCK_STRING
+            if i == 7 and weekday < 4 and j - 3 == weekday:
+                nboard[i][j] = LOCK_STRING
+            if i == 8 and weekday > 3 and j + 1 == weekday:
+                nboard[i][j] = LOCK_STRING
+    return nboard
+
 def plot():
     pass
 def main():
     pass
+
+# printBk(rotate(bk2,-2))
