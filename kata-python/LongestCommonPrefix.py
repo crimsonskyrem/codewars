fd69e7d359742e694b7e9b67252ab09e888dfd65
crimsonskyrem <crimsonsky.rem@gmail.com>
2022-03-23 14:15:27 +0800

    no flags

--- kata-python/LongestCommonPrefix.py
+++ kata-python/LongestCommonPrefix.py
@@ -4,16 +4,13 @@ def longestCommonPrefix(strs) -> str:
     length = min(map(len, strs))
     if length == 0:
         return ''
-    match = [False] * length
     pos = 0
     while pos < length:
         for i in range(1, len(strs)):
             if strs[i][pos] != strs[0][pos]:
-                return ''.join(map(lambda x: x[0] if x[1] else '', zip(strs[0], match)))
-        match[pos] = True
+                return strs[0][:pos]
         pos += 1
-    return ''.join(map(lambda x: x[0] if x[1] else '', zip(strs[0], match)))
-
+    return strs[0][:pos]
 
 print(longestCommonPrefix(["flower", "flow", "flight"]) == "fl")
 print(longestCommonPrefix(["dog","racecar","car"]) == "")
